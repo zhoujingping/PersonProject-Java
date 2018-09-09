aa5999c58333660b090933e221a68ec69ac45ee0
wyz0918 <wuyizhao2008@126.com>
2018-09-09 15:35:57 +0800

    fix:修复了词频统计计算单词数的BUG

--- 031602335/src/Main.java
+++ 031602335/src/Main.java
@@ -1,82 +1,95 @@
-package workOut;
+
 
 import java.io.*;
 import java.util.*;
-import java.util.regex.*;
 
 public class Main {
 
-	static ArrayList<String> strArray = new ArrayList<String>();
-	static int lines = 0;//行数
-	static int characters = 0;//字符数
-	static int words = 0;//单词数
+	static ArrayList<String> strArray = new ArrayList<String> ();//存储每一行的字符串
+	static ArrayList<String> wordArray2 = new ArrayList<String> ();//存储分割后的字符串
+	static int lines = 0;// 行数
+	static int characters = 0;// 字符数
+	static int words = 0;// 单词数
 	static HashMap<String, Integer> hashMap = new HashMap<String, Integer>();// 用于储存单词及其个数
-	static ArrayList<Map.Entry<String, Integer>> list; //用于排序
+	static ArrayList<Map.Entry<String, Integer>> list; // 用于储存entry
 
 	public static void main(String[] args) throws IOException {
 
-		readFile(args);
-		workOut();
-		writeFile();
-
-	}
-
-	// hashMap中按照value的降序和key的升序排序
-	
-	public static void sortWords(HashMap<String, Integer> hashMap, ArrayList<Map.Entry<String, Integer>> list) {
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-			public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-				int re = e2.getValue().toString().compareTo(e1.getValue().toString());
-				if (re != 0) {
-					return re;
-				} else {
-					return e2.getKey().toString().compareTo(e1.getKey());
-				}
-			}
-		});
+		Main m = new Main();
+		m.readFile(args);
+		m.workOut();
+		m.sortWords();
+		m.writeFile();
 
 	}
 
 	// 读入文件
-	
-	public static void readFile(String[] args) throws IOException {
 
-		InputStreamReader reader = new InputStreamReader(new FileInputStream(args[0]));
-		BufferedReader br = new BufferedReader(reader);
-		String str = null;
+	public void readFile(String[] args) throws IOException {
 
-		while ((str = br.readLine()) != null) {
-			strArray.add(str);
+		if (args != null && args.length > 0) {
+
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(args[0]));
+			BufferedReader br = new BufferedReader(reader);
+			String str = null;
+
+			while ((str = br.readLine()) != null) {
+				String[] wordArray = str.split("\\s*[^a-zA-Z0-9]+");
+				for(String word :wordArray) {
+					wordArray2.add(word);
+				}
+				strArray.add(str);
+			}
+			
+			reader.close();
 		}
-		reader.close();
 	}
 
 	// 实现计算字符数、行数、单词总数，出现次数
 
-	public static void workOut() {
+	public void workOut() {
 
-		Pattern pattern = Pattern.compile("[a-zA-Z]{4,}[a-zA-Z0-9]*");
-		Matcher matcher;
-		String word = null;
-		for (int i = 0; i < strArray.size(); i++) {
-			characters += strArray.get(i).length();// 计算字符数
-			lines++;// 计算行数
-			matcher = pattern.matcher(strArray.get(i));
-			while (matcher.find()) {
-				words++; // 计算单词总数
-				word = matcher.group();
-				if (hashMap.containsKey(word)) {
-					hashMap.put(word, hashMap.get(word) + 1);// 计算单词出现次数
-				} else {
-					hashMap.put(word, 1);
-				}
-			}
+		
+		
+		for (int i =0;i<strArray.size();i++) {
+			Main.characters += Main.strArray.get(i).length();// 计算字符数
+			Main.lines++;// 计算行数
+			
 		}
+		for(String word : wordArray2) {
+			if(word.matches("[a-zA-Z]{4,}[a-zA-Z0-9]*")) {
+				Main.words++; // 计算单词总数
+				word = word.toLowerCase();
+				if (Main.hashMap.containsKey(word)) {
+					Main.hashMap.put(word, hashMap.get(word) + 1);// 计算单词出现次数
+				} else {
+					Main.hashMap.put(word, 1);
+				}
+				
+			}
+			
+			
+		}
+		Main.list = new ArrayList<Map.Entry<String, Integer>>(hashMap.entrySet());
+	}
+
+	// hashMap中按照value的降序和key的升序排序
+
+	public void sortWords() {
+		Collections.sort(Main.list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+				int re = e2.getValue().toString().compareTo(e1.getValue().toString());
+				return (re == 0) ? e1.getKey().compareTo(e2.getKey()) : re;
+
+			}
+
+		});
+
 	}
 
 	// 写入文件
-	
-	public static void writeFile() throws IOException {
+
+	public void writeFile() throws IOException {
 
 		File f = new File("result.txt");
 		f.createNewFile();
@@ -86,12 +99,12 @@ public class Main {
 		System.out.println("characters: " + characters);
 		System.out.println("words: " + words);
 		System.out.println("lines: " + lines);
-		list = new ArrayList<Map.Entry<String, Integer>>(hashMap.entrySet());
-		sortWords(hashMap, list);
-		for (int i = 0; i < 10; i++) {
 
-			System.out.println(list.get(i).getKey().toLowerCase() + ": " + list.get(i).getValue());
+		for (int i = 0; i < 10 && i < list.size(); i++) {
+
+			System.out.println(list.get(i).getKey() + ": " + list.get(i).getValue());
 		}
+
 	}
 
 }
