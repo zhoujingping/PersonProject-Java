9a4f05577847aeffafc4dccd7725dff084cbe3f6
xshl <s839806383@163.com>
2018-09-10 00:31:29 +0800

    read

---
+++ 031602636/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+ * 主程序，可以用命令行接受参数
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String path = scanner.nextLine();
+		
+		System.out.println(path);
+	}
+}
