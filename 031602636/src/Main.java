dc5a9ed566d51e749719cdf9f6b901f203a95ed9
xshl <s839806383@163.com>
2018-09-12 00:01:38 +0800

    行数，字符数

--- 031602636/src/Main.java
+++ 031602636/src/Main.java
@@ -8,10 +8,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String path = scanner.nextLine();
-		
-		System.out.println(path);
 		File file = new File(path);
 		int charcount = charCount.charCount(file);
-		System.out.println(charcount);
+		System.out.println("characters:" + charCount.charCount(file));
+		System.out.println("lines:" + lineCount.lineCount(file));
 	}
 }
--- 031602636/src/charCount.java
+++ 031602636/src/charCount.java
@@ -11,8 +11,16 @@ public class charCount {
 			if (file.isFile() && file.exists()) {	//检测文件名是否正常或文件是否存在
 				InputStreamReader isReader = new InputStreamReader(new FileInputStream(file),"utf-8");
 				BufferedReader bReader = new BufferedReader(isReader);
-				while(bReader.read() != -1)
-					charcount++;
+				int value = 0;
+				int value2 = 0;
+				while((value = bReader.read()) != -1) {
+					if (value >= 0 && value<=255) {
+						charcount++;
+						value2 = value;
+					}
+				}
+				charcount = charcount - lineCount.lineCount(file);
+				//由于回车占两个字符
 				bReader.close();
 			} else {
 				System.out.println("文件不存在");
