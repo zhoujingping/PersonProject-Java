96a7c5f75de08e09bbe197cb6efea34ce0339d5e
wyz0918 <wuyizhao2008@126.com>
2018-09-12 00:42:06 +0800

    fix:修复单词数以及字符数计算BUG，代码优化

--- 031602335/src/Main.java
+++ 031602335/src/Main.java
@@ -1,101 +1,73 @@
-
-
-
+package workOut;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	static ArrayList<String> strArray = new ArrayList<String> ();//存储每一行的字符串
-	static ArrayList<String> wordArray2 = new ArrayList<String> ();//存储分割后的字符串
-	static int lines = 0;// 行数
-	static int characters = 0;// 字符数
-	static int words = 0;// 单词数
-	static HashMap<String, Integer> hashMap = new HashMap<String, Integer>();// 用于储存单词及其个数
-	static ArrayList<Map.Entry<String, Integer>> list; // 用于储存entry
+	
+	private static int lines = 0;// 行数
+	private static int characters = 0;// 字符数
+	private static int words = 0;// 单词数
+	private static ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>();
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException  {
 
 		Main m = new Main();
-		m.readFile(args);
-		m.workOut();
-		m.sortWords();
+		File f = m.readFile(args);
+		
+		Main.characters = Lib.countChars(f);
+		Main.lines = Lib.countLines(f);
+		Main.words = Lib.countWords(f);
+		Main.list = Lib.countTop10(f);
+
 		m.writeFile();
+
 
 	}
 
 	// 读入文件
 
-	public void readFile(String[] args) throws IOException {
+	public File readFile(String[] args) {
 
-		if (args != null && args.length > 0) {
-
-			InputStreamReader reader = new InputStreamReader(new FileInputStream(args[0]));
-			BufferedReader br = new BufferedReader(reader);
-			String str = null;
-
-			while ((str = br.readLine()) != null) {
-				String[] wordArray = str.split("\\s*[^a-zA-Z0-9]+");
-				for(String word :wordArray) {
-					wordArray2.add(word);
-				}
-				strArray.add(str);
-			}
-			
-			reader.close();
+		String fileName = null;
+		File f = null;
+		
+		if(args.length == 0 || args == null) {
+			Scanner scan = new Scanner(System.in);
+			fileName = scan.nextLine();
+			scan.close();
 		}
+		else {
+			fileName = args[0];
+		}
+		
+		f = new File(fileName);
+		if(!f.exists()) {
+			System.out.println(f+"文件不存在");
+			System.exit(0);
+		}
+		
+		return f;
+		
 	}
 
-	// 实现计算字符数、行数、单词总数，出现次数
 
-	public void workOut() {
-
-		for (int i =0;i<strArray.size();i++) {
-			Main.characters += Main.strArray.get(i).length();// 计算字符数
-			if(!strArray.get(i).trim().isEmpty()) {
-				Main.lines++;
-			}// 计算行数
-			
-		}
-		for(String word : wordArray2) {
-			if(word.matches("[a-zA-Z]{4,}[a-zA-Z0-9]*")) {
-				Main.words++; // 计算单词总数
-				word = word.toLowerCase();
-				if (Main.hashMap.containsKey(word)) {
-					Main.hashMap.put(word, hashMap.get(word) + 1);// 计算单词出现次数
-				} else {
-					Main.hashMap.put(word, 1);
-				}
-				
-			}
-			
-			
-		}
-		Main.list = new ArrayList<Map.Entry<String, Integer>>(hashMap.entrySet());
-	}
-
-	// hashMap中按照value的降序和key的升序排序
-
-	public void sortWords() {
-		Collections.sort(Main.list, new Comparator<Map.Entry<String, Integer>>() {
-			public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-				int re = e2.getValue().toString().compareTo(e1.getValue().toString());
-				return (re == 0) ? e1.getKey().compareTo(e2.getKey()) : re;
-
-			}
-
-		});
-
-	}
 
 	// 写入文件
 
-	public void writeFile() throws IOException {
+	public void writeFile()  {
 
 		File f = new File("result.txt");
-		f.createNewFile();
-		FileOutputStream fos = new FileOutputStream(f);
+		FileOutputStream fos = null;
+		try {
+			f.createNewFile();
+			fos = new FileOutputStream(f);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		PrintStream ps = new PrintStream(fos);
 		System.setOut(ps);
 		System.out.println("characters: " + characters);
